99075c616717596ef2d9ca5ae93c69026ea2d88e
vinhhoanglong <longvinhvhbk@gmail.com>
2024-11-13 22:00:20 +0700

    update

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ import argparse
 import os
 
 
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])       
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)

5bdbf4b522b1d7599b4982637b93159aea5405b3
vinhhoanglong <longvinhvhbk@gmail.com>
2024-11-13 21:55:24 +0700

    update

--- Test.py
+++ Test.py
@@ -9,7 +9,7 @@ import os
 
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])       
-testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
 
 def main(testloader, model_path):
@@ -24,7 +24,7 @@ def main(testloader, model_path):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()   
-    print(f'Accuracy of the network on the 10000 test images: {100 * correct / total:.2f}%')
+    print(f'Accuracy of the network on the test images: {100 * correct / total:.2f}%')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- Train.py
+++ Train.py
@@ -14,7 +14,7 @@ criterion = nn.CrossEntropyLoss()
 
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])       
-trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 

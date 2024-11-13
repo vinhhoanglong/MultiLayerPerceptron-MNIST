6f116d62a99439316fd4bcf58fb037a70a3fe0c1
vinhhoanglong <longvinhvhbk@gmail.com>
2024-11-13 22:48:17 +0700

    update

--- Test.py
+++ Test.py
@@ -3,33 +3,41 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import argparse
-import os
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])       
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  
+])       
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_path', type=str, default='model/mlp_20241113_224236.save', help='Path to the trained model file')
+parser.add_argument('--batch_size', type=int, default=64, help='Batch size for testing')
+args = parser.parse_args()
+
+
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False)
 
 def main(testloader, model_path):
-    model = MLP().to(device)
-    model.load_state_dict(torch.load(model_path))
+    model = MLP().to(device)  
+    model.load_state_dict(torch.load(model_path, map_location=device))  
     model.eval()
+    
     correct = 0
     total = 0
     with torch.no_grad():
         for images, labels in testloader:
-            images, labels = images.to(device), labels.to(device) 
+            images, labels = images.to(device), labels.to(device)  
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()   
+            
     print(f'Accuracy of the network on the test images: {100 * correct / total:.2f}%')
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default='model/mlp_20241006_220807.save')
-    args = parser.parse_args()
     main(testloader, args.model_path) 
-
--- Train.py
+++ Train.py
@@ -7,16 +7,25 @@ import torch
 import argparse
 import datetime
 
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = MLP().to(device) 
 criterion = nn.CrossEntropyLoss()
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])       
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_epochs', type=int, default=5)
+parser.add_argument('--learning_rate', type=float, default=0.01)
+parser.add_argument('--optimizer', type=str, choices=['SGD', 'Adam'], default='SGD', help='Optimizer to use: SGD or Adam')
+parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training')
 
+args = parser.parse_args()
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  
+])       
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True)  
 
 def main(model, trainloader, criterion, optimizer, num_epochs):
     for epoch in range(num_epochs):
@@ -30,13 +39,13 @@ def main(model, trainloader, criterion, optimizer, num_epochs):
             optimizer.step()
             
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+    
     torch.save(model.state_dict(), f'model/mlp_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.save')
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--num_epochs', type=int, default=5)
-    parser.add_argument('--learning_rate', type=float, default=0.01)
-    args = parser.parse_args()
-    optimizer = optim.SGD(model.parameters(), lr = args.learning_rate)
-    main(model, trainloader, criterion, optimizer, args.num_epochs)
 
+if args.optimizer == 'SGD':
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate)
+elif args.optimizer == 'Adam':
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+
+main(model, trainloader, criterion, optimizer, args.num_epochs)
